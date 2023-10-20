1408041746ee88c656887adf737711dfcb39e402
Kate Zaharova <15071981ktz@gmail.com>
2023-10-20 23:28:32 +0300

    evening

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -16,7 +16,7 @@ export default function Home() {
     return (
         <div style={styles.container}>
             <h1 style={styles.title}>
-                Have a nice day, mentor!
+                Good evening, mentor!
             </h1>
         </div>
     );
