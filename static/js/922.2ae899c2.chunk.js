bf20a68917dee76b085dafd2a2ccd6bf76153ef4
KateZaharova <124520175+KateZaharova@users.noreply.github.com>
2023-10-20 10:17:39 +0000

    Deploying to gh-pages from @ KateZaharova/goit-react-hw-08-phonebook@3114117f53b817a07ca8c7ca6f3a2c1e6e07dc6c 🚀

---
+++ static/js/922.2ae899c2.chunk.js
@@ -0,0 +1,2 @@
+"use strict";(self.webpackChunkreact_homework_template=self.webpackChunkreact_homework_template||[]).push([[922],{8922:function(e,t,n){n.r(t),n.d(t,{default:function(){return i}});var c=n(3329),r={container:{minHeight:"calc(100vh - 50px)",display:"flex",alignItems:"center",justifyContent:"center"},title:{fontWeight:500,fontSize:48,textAlign:"center"}};function i(){return(0,c.jsx)("div",{style:r.container,children:(0,c.jsx)("h1",{style:r.title,children:"No such contacts here, unfortunately!"})})}}}]);
+//# sourceMappingURL=922.2ae899c2.chunk.js.map
